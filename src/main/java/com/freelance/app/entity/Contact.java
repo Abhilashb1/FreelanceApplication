81ec0ec7ea5ec569b182b06c051ae43931e5c70b
abhilash <abhilash.udupi@gmail.com>
2020-07-15 00:57:40 +0530

    initial commit

---
+++ src/main/java/com/freelance/app/entity/Contact.java
@@ -0,0 +1,34 @@
+package com.freelance.app.entity;
+
+import java.util.List;
+
+public class Contact {
+
+    private  List<Phone> phoneList;
+    private  List<Address> addressList ;
+    private  List<Email> emailList;
+
+    public List<Phone> getPhoneList() {
+        return phoneList;
+    }
+
+    public void setPhoneList(List<Phone> phoneList) {
+        this.phoneList = phoneList;
+    }
+
+    public List<Address> getAddressList() {
+        return addressList;
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList = addressList;
+    }
+
+    public List<Email> getEmailList() {
+        return emailList;
+    }
+
+    public void setEmailList(List<Email> emailList) {
+        this.emailList = emailList;
+    }
+}
---
+++ src/main/java/com/freelance/app/entity/User.java
@@ -0,0 +1,12 @@
+package com.freelance.app.entity;
+
+import java.util.List;
+
+public class User {
+
+    private  String userName;
+    private  String userType;
+    private  List<Contact> contactList;
+}
+
+
