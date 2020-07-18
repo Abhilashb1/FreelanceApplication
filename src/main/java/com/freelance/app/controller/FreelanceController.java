18d57fecc25ee00927224c97b8aca686b658b97d
abhilash <abhilash.udupi@gmail.com>
2020-07-18 16:07:06 +0530

    Exception commit

--- src/main/java/com/freelance/app/controller/FreelanceController.java
+++ src/main/java/com/freelance/app/controller/FreelanceController.java
@@ -1,8 +1,12 @@
 package com.freelance.app.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 
@@ -12,6 +16,15 @@ public class FreelanceController {
     public String getClients()
     {
         return "ABC";
+    }
+
+    @GetMapping(path = "/clients/{id}")
+    public String getClient()
+    {
+        URI location= ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand().toUri();
+        ResponseEntity.created(location);
+        return "ABC";
+
     }
 
     @PostMapping(path = "/addUser")
